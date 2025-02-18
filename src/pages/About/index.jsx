0d8f3e699c5323a182a4b5d566b1756d411a36f8
jdevis <josephine.roubertie@gmail.com>
2025-02-18 16:02:02 +0100

    dictionnary optimisation

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,17 +1,19 @@
-import Banner from "../../components/Banner"
-import Dropdown from "../../components/Dropdown"
-import { dictionnaries } from "../../data/Dictionnary"
+import Banner from '../../components/Banner'
+import Dropdown from '../../components/Dropdown'
+import Dictionnaries from '../../data/Dictionnary.json'
 import './about.scss'
 
 const About = () => {
+    const lang = 'fr' // 'en' for english
     return (
         <>
             <main>
                 <Banner page='about' />
                 <div className="about">
-                    {dictionnaries.map((dictionnary) => (
+                    {Dictionnaries.map((dictionnary) => (
                         <div key={dictionnary.id} className="about-dropdown">
-                            <Dropdown title={dictionnary.fr.title} datas={dictionnary.fr} />
+                            {lang === 'fr' && <Dropdown title={dictionnary.fr.title} datas={dictionnary.fr} />}
+                            {lang === 'en' && <Dropdown title={dictionnary.en.title} datas={dictionnary.en} />}
                         </div>
                     ))}
                 </div>
